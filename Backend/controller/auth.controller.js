7fa085072dc454fb608fcdfa53e3e524991c4e36
Harsh saini <harshsainib@gmail.com>
2026-06-10 10:29:13 +0530

    Fix JWT expiration variable name

--- Backend/controller/auth.controller.js
+++ Backend/controller/auth.controller.js
@@ -6,7 +6,7 @@ import blacklistTokenModel from "../models/blacklist.model.js";
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES,
+    expiresIn: process.env.JWT_EXPIRES_IN,
   });
 };
 
